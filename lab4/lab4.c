d59d6dcb91cab300621025421bbe3dc1d2277ba7
Tiago Barbosa <up202004926@fe.up.pt>
2022-05-17 16:09:24 +0100

    gesture

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -11,6 +11,9 @@ extern uint32_t total_packets;
 extern bool reading_error;
 extern int mouse_counter;
 extern int counter;
+extern bool gesture_detected;
+extern mouse_event_t event;
+extern bool packet_read;
 
 // Any header files included below this line should have been created by you
 
@@ -138,8 +141,50 @@ int (mouse_test_async)(uint8_t idle_time) {
 }
 
 int (mouse_test_gesture)(uint8_t x_len, uint8_t tolerance) {
-    /* To be completed */
-    printf("%s: under construction\n", __func__);
-    return 1;
+    if(enable_mouse_data()!=0){
+        return 1;
+    }
+    int ipc_status;
+    u_int8_t bit_no = 1;
+    u_int32_t irq_set = BIT(bit_no);
+    message msg;
+    mouse_subscribe(&bit_no);
+
+    while (!gesture_detected) { 
+        int r = driver_receive(ANY, &msg, &ipc_status);
+        if (r != 0) {
+            printf("driver_receive failed with: %d", r);
+            continue;
+        }
+        if (is_ipc_notify(ipc_status)) { 
+            switch (_ENDPOINT_P(msg.m_source)) {
+                case HARDWARE:                             
+                if (msg.m_notify.interrupts & irq_set) {
+                    mouse_ih();
+                    if (reading_error == true && mouse_counter != 2) {
+                        continue;
+                    }
+                    else if (reading_error == true && mouse_counter == 2) {
+                        reading_error = false;
+                    }
+                    else{
+                        organize_packets();
+                        if(packet_read){
+                            printf("%d\n",event.type);
+                            mouse_check_pattern(event,x_len,tolerance);
+                        }
+                    }
+                }
+                break;
+                default: break;
+            }
+        }
+    }
+    mouse_unsubscribe();
+
+    if (disable_mouse_data()){  
+        return 1;
+    }
+    return 0;
 }
 
